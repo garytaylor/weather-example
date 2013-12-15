dfaa9eb8ad150e85acaa896217a3274e1c92c7e6
Gary Taylor <gary.taylor@hismessages.com>
2013-12-15 12:17:51 +0000

    The feature is now running, and as expected, the tests are failing as we currently do not have a '/' route, nor any sort of home page developed.

--- features/support/selectors.rb
+++ features/support/selectors.rb
@@ -1,13 +1,15 @@
 module Weather
   module Selectors
-    def todays_forecast_widget
-      '.widget.todays_forecast'
-    end
-    def weekly_forecast_widget
-      '.widget.weekly_forecast'
-    end
-    def login_bar
-      '.widget.login_bar'
+    class << self
+      def todays_forecast_widget
+        '.widget.todays_forecast'
+      end
+      def weekly_forecast_widget
+        '.widget.weekly_forecast'
+      end
+      def login_bar
+        '.widget.login_bar'
+      end
     end
   end
 end
