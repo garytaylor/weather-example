2e528e203ac335e63860546ac244edf7b3f65dff
Gary Taylor <gary.taylor@hismessages.com>
2013-12-15 13:22:02 +0000

    Added method to delete cookies

--- features/support/cookies.rb
+++ features/support/cookies.rb
@@ -1,3 +1,5 @@
+#Deletes all cookies.  This can be done during a step, or possibly before every scenario if it is not too expensive
 def remove_all_cookies
-
+  browser = Capybara.current_session.driver.browser
+  browser.manage.delete_all_cookies
 end
